3323aea8eb1298772c899a7103c3e916f47420ed
samrdh <samrdhgoa@gmail.com>
2018-11-13 17:30:25 +0530

    icons added

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -14,20 +14,20 @@ export class WelcomePage {
     {
       title: "Welcome to YASH",
       description: "A place for businesses to Grow & Clients to Multiply with no hastle.",
-      image: "assets/imgs/yashgroupgoa.png",
+      image: "assets/imgs/yashgrp.png",
       color:"#8dae37",
     },
     {
       title: "Step 1",
       description: "Login as a business associate to get started. <br><small>(You need to be registered through our website to login. If your not yet a member please follow the web link below to get yourself registered)</small>.",
       link:"www.yashgoa.com",
-      image: "assets/img/ica-slidebox-img-2.png",
+      image: "assets/imgs/guyonpc.png",
       color:"#dbae0a",
     },
     {
       title: "Step 2",
       description: "Once logged in as a business associate, keep the customer login form opened to the customers so, that they can get their bills verified",
-      image: "assets/img/ica-slidebox-img-3.png",
+      image: "assets/imgs/form-icon.png",
       color:"#3fa6a0",
     }
   ];
