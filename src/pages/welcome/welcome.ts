d19d3300d92ab9c51e73a06d5bdc81c873766dd2
samrdh <samrdhgoa@gmail.com>
2018-11-13 14:56:12 +0530

    fonts n stuff

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -13,18 +13,22 @@ export class WelcomePage {
   slides = [
     {
       title: "Welcome to YASH",
-      description: "A place for businesses to <b>Grow</b> & Clients to  <b>Multiply</b> with no hastle.",
+      description: "A place for businesses to Grow & Clients to Multiply with no hastle.",
       image: "assets/imgs/yashgroupgoa.png",
+      color:"#8dae37",
     },
     {
       title: "Step 1",
-      description: "Login as a business associate to get started. <br><small><b>(You need to be registered through our website to login. If your not yet a member please follow the web link below to get yourself registered)</b></small>.<br><a href='http://www.yashgoa.com'>www.yashgoa.com</a>",
+      description: "Login as a business associate to get started. <br><small>(You need to be registered through our website to login. If your not yet a member please follow the web link below to get yourself registered)</small>.",
+      link:"www.yashgoa.com",
       image: "assets/img/ica-slidebox-img-2.png",
+      color:"#dbae0a",
     },
     {
       title: "Step 2",
-      description: "Once logged in as a business associate, <b>keep the customer login form opened to the customers</b> so, that they can get their bills verified",
+      description: "Once logged in as a business associate, keep the customer login form opened to the customers so, that they can get their bills verified",
       image: "assets/img/ica-slidebox-img-3.png",
+      color:"#3fa6a0",
     }
   ];
 
